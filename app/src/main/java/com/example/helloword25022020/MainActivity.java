53186b600ed1bf139fe653e33a3c881ca4228acf
phatdroid94 <phatdroid94@gmail.com>
2020-03-10 21:38:26 +0700

    update

--- app/src/main/java/com/example/helloword25022020/MainActivity.java
+++ app/src/main/java/com/example/helloword25022020/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.helloword25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 1 : Khai bao bien
+    Button mBtnShowAlert ;
+    TextView mTvTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 2 : anh xa
+        mBtnShowAlert = findViewById(R.id.buttonShow);
+        mTvTitle = findViewById(R.id.textviewTitle);
+
+        // 3 : chay code
+        //In ra tren may ao thong bao
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
