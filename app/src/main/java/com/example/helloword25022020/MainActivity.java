f95112064db9385a008f26f32254f429acf98a8d
phatdroid94 <phatdroid94@gmail.com>
2020-03-12 20:40:39 +0700

    finish

--- app/src/main/java/com/example/helloword25022020/MainActivity.java
+++ app/src/main/java/com/example/helloword25022020/MainActivity.java
@@ -3,15 +3,20 @@ package com.example.helloword25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
 
 public class MainActivity extends AppCompatActivity {
 
     // 1 : Khai bao bien
     Button mBtnShowAlert ;
     TextView mTvTitle;
+    String a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         // 3 : chay code
         //In ra tren may ao thong bao
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        //built in function
+//        Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+        // Kiem tra du lieu trong he thong
+
+//        Log.d("BBB",a);
+        // ctrl + P : goi y tham so truyen vao
+        mBtnShowAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Hello",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
